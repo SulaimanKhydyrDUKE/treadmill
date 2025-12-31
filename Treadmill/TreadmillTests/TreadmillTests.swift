60c890faabd06515bf2aef9117857db541cf5cd1
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-31 02:03:32 -0600

    Fixed folder tracking: converted from submodule to normal directory

---
+++ Treadmill/TreadmillTests/TreadmillTests.swift
@@ -0,0 +1,17 @@
+//
+//  TreadmillTests.swift
+//  TreadmillTests
+//
+//  Created by Sulaiman Khydyr uulu on 12/31/25.
+//
+
+import Testing
+@testable import Treadmill
+
+struct TreadmillTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
