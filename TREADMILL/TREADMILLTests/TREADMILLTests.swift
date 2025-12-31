7113e15bcffecd4165a896a98347f4dab83fedad
Sulaiman Khydyr uulu <sulaiman.khydyruulu@duke.edu>
2025-12-31 04:32:30 -0600

    Enhanced UserProfile model with additional fields and improved validation logic, updated related views for better user experience.

---
+++ TREADMILL/TREADMILLTests/TREADMILLTests.swift
@@ -0,0 +1,17 @@
+//
+//  TREADMILLTests.swift
+//  TREADMILLTests
+//
+//  Created by Sulaiman Khydyr uulu on 12/31/25.
+//
+
+import Testing
+@testable import TREADMILL
+
+struct TREADMILLTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
